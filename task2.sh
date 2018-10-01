680748ab93a386d0e365b675ef85208bb134acd3
Shuyue Li <susie.lishuyue@gmail.com>
2018-10-01 13:06:57 -0400

    Command for task 3 added

--- task2.sh
+++ task2.sh
@@ -8,6 +8,7 @@ cat wages.csv | sort -n -t , -k 4 | head -n 2 | tail -n 1
 echo "The number of females in top ten earners:"
 cat wages.csv | sort -n -r -t , -k 4 | head -n 11 | tail -n 10 | grep female | wc -l
 cat wages.csv | cut -f 1,2 -d , | sort | sed -E 's/,/ /g' | sort -k 2 -n | uniq > unique.txt 
-var1 = $(cat wages.csv | cut -f 3,4 -d , | grep -E ^12 | sort -n -k 2  | sed -E 's/,/ /g' | sort -k 2 -n | head -n 1 | cut -f 2 -d " ") 
-var2 = $(cat wages.csv | cut -f 3,4 -d , | grep -E ^16 | sort -n -k 2  | sed -E 's/,/ /g' | sort -k 2 -n | head -n 1 | cut -f 2 -d " ")
-echo "$var2 - $var1" | bc  
+var1=$(cat wages.csv | cut -f 3,4 -d , | grep -E ^12 | sort -n -k 2  | sed -E 's/,/ /g' | sort -k 2 -n | head -n 1 | cut -f 2 -d " ") 
+var2=$(cat wages.csv | cut -f 3,4 -d , | grep -E ^16 | sort -n -k 2  | sed -E 's/,/ /g' | sort -k 2 -n | head -n 1 | cut -f 2 -d " ")
+echo "Difference in minimum wages between 12 and 16 years of education:"
+echo "$var2 - $var1" | bc
