becbfa1a5ee2d476e067a2815d773895fb17a15e
Shuyue Li <susie.lishuyue@gmail.com>
2018-10-01 12:44:06 -0400

    Task 2 script added

---
+++ task2.sh
@@ -0,0 +1,9 @@
+# This script is for IBC_EX5 Task 2
+# Usage: bash task2.sh .
+
+echo "Highest earner:"
+cat wages.csv | sort -n -r -t , -k 4 | head -n 1
+echo "Lowest earner:"
+cat wages.csv | sort -n -t , -k 4 | head -n 2 | tail -n 1
+echo "The number of females in top ten earners:"
+cat wages.csv | sort -n -r -t , -k 4 | head -n 11 | tail -n 10 | grep female | wc -l
